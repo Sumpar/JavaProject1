8812b0e82f41d5119830e8cce26a85d79ceac86a
Sumin Park <D13122541@mydit.ie>
2017-04-23 23:33:47 +0100

    First update
    First update of Java project that I am working on

---
+++ src/Project1/control.java
@@ -0,0 +1,13 @@
+package Project1;
+
+import Project1.Screen;
+
+public class control {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Screen newgui = new Screen(null);
+	}
+
+}
